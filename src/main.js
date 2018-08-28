1ca8767f2aeb4ea5e06ababdb6b88a776909c8f5
Charlie Jackson <charlie@charliejackson.com>
2018-08-28 12:29:40 +0300

    Fixed exports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import linkAllNameSpacedDependencies from './linking/linkAllNameSpacedDependencies';
+import unlinkAllNameSpacedDependencies from './linking/linkAllNameSpacedDependencies';
+
 export { registerCommand, processCommands } from './commands';
-export linkAllNameSpacedDependencies from './linking/linkAllNameSpacedDependencies';
-export unlinkAllNameSpacedDependencies from './linking/linkAllNameSpacedDependencies';
 export { getProjectDir } from './project';
+export { linkAllNameSpacedDependencies, unlinkAllNameSpacedDependencies };
