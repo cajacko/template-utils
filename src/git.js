2c9c2727d133eea940d6fa4392b84cfe65f6c64a
Charlie Jackson <charlie@charliejackson.com>
2018-08-29 12:28:53 +0300

    Starting adding git funcs

--- src/git.js
+++ src/git.js
@@ -1,5 +1,6 @@
 // @flow
 
+import simpleGit from 'simple-git/promise';
 import getIsGit from 'is-git-repository';
 
 const git = {
@@ -10,8 +11,28 @@ const git = {
 
     return Promise.reject(new Error(`Supplied directory is not a git repo: ${dir}`));
   },
-  getLastVersionTag: dir => Promise.resolve('v1.0.0'),
-  getAllCommitsSinceTag: (dir, tag) => Promise.resolve([{}]),
+  getLastVersionTag: dir =>
+    simpleGit(dir)
+      .tags()
+      .then((tags) => {
+        const lastVersionTag = tags.all
+          .reverse()
+          .find(tag => !!tag.match(/v[0-9]+\.[0-9]+\.[0-9]+/));
+
+        if (!lastVersionTag) {
+          throw new Error(`Could not find the latest version tag in ${dir}`);
+        }
+
+        return lastVersionTag;
+      }),
+  getAllCommitsSinceTag: (dir, tag) =>
+    simpleGit(dir)
+      .log({
+        from: tag,
+        to: 'HEAD',
+      })
+      // .log(`${tag}..HEAD`)
+      .then(logs => logs.all),
   commit: (dir, message) => Promise.resolve(),
   push: dir => Promise.resolve(),
   tag: (dir, tag, message) => Promise.resolve(),
